5c6006e54e53a79a0cb79cafc459c20f0f475143
KuhakuAlves <ricardo.rodrigues.alves@hotmail.com>
2019-08-03 12:24:25 -0300

    Alterções novas de Controller

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,10 +2,23 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@ManyToOne
 	private Sala sala;
 	
+	@ManyToOne
 	private Filme filme;
 	
 	private LocalTime horario;
@@ -34,6 +47,8 @@ public class Sessao {
 		this.horario = horario;
 	}
 	
-	
+   public Sessao() {
+	   
+   }
 
 }
---
+++ src/main/java/br/com/caelum/ingresso/model/form/SessaoForm.java
@@ -0,0 +1,59 @@
+package br.com.caelum.ingresso.model.form;
+
+import java.time.LocalTime;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import br.com.caelum.ingresso.dao.FilmeDao;
+import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.model.Sessao;
+
+public class SessaoForm {
+
+	@DateTimeFormat(pattern = "HH:mm")
+	@NotNull
+	private LocalTime horario;
+	
+	@NotNull
+	private Integer salaId;
+
+	@NotNull
+	private Integer filmeId;
+
+	public LocalTime getHorario() {
+		return horario;
+	}
+
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+	
+	public Integer getSalaId() {
+		return salaId;
+	}
+
+	public void setSalaId(Integer salaId) {
+		this.salaId = salaId;
+	}
+
+	public Integer getFilmeId() {
+		return filmeId;
+	}
+
+	public void setFilmeId(Integer filmeId) {
+		this.filmeId = filmeId;
+	}
+
+	public Sessao toSessao(SalaDao salaDao, FilmeDao filmeDao) {
+		Sessao sessao = new Sessao(); 
+		
+		sessao.setHorario(this.horario);
+		sessao.setFilme(filmeDao.findOne(this.salaId));
+		sessao.setSala(salaDao.findOne(this.filmeId));
+		
+		return sessao;
+	}
+
+}
