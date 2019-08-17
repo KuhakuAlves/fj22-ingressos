f6281eab7b8a6a975f8f2b7c2bf43829121e5fb2
KuhakuAlves <ricardo.rodrigues.alves@hotmail.com>
2019-08-17 17:57:50 -0300

    Terminando o curso

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.ModelAndView;
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Carrinho;
 import br.com.caelum.ingresso.model.DetalhesDoFilme;
 import br.com.caelum.ingresso.model.ImagemCapa;
 import br.com.caelum.ingresso.model.Sessao;
@@ -40,6 +41,9 @@ public class SessaoController {
 	
     @Autowired
     private OmdbClient client;
+    
+    @Autowired
+    private Carrinho carrinho;
 	
 	@GetMapping("/admin/sessao")
 	public ModelAndView form( @RequestParam("salaId") Integer id, SessaoForm form ) {
@@ -85,6 +89,7 @@ public class SessaoController {
 		Optional<ImagemCapa> imagemCapa = client.request(sessao.getFilme(), ImagemCapa.class);
 		
 		mv.addObject("sessao", sessao);
+		mv.addObject("carrinho", carrinho);
 		mv.addObject("imagemCapa", imagemCapa.orElse(new ImagemCapa()));
 		mv.addObject("tiposDeIngressos", TipoDeIngresso.values());
 		return mv;
